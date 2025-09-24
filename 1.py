2cdb7025515a3e2a654ced053425f2d456845a1a
Axiza2k5 <46486645+Axiza2k5@users.noreply.github.com>
2025-09-24 22:06:27 +0700

    1: AC 50%

--- 1.py
+++ 1.py
@@ -13,10 +13,19 @@ class Solution:
         return -1
         
     def twoSum(self, nums, target: int):
-        for n, i in enumerate(nums):
-            if self.binary_search(nums, target - i) != -1:
-                return (n,self.binary_search(nums, target - i))
-        return None
+        seen = {}
+        for i, n in enumerate(nums):
+            if n in seen:
+                seen[n].append(i)
+            else:
+                seen[n] = [i]
+        
+        for n in nums:
+            index = seen[n].pop(0)
+            if target-n in seen and len(seen[target-n]) !=0:
+                return (index, seen[target-n].pop())
+            seen[n].append(index)
         
 
-print(Solution().twoSum([2,7,11,15],9))
+# print(Solution().twoSum([2,7,11,15],9))
+print(Solution().twoSum([3,2,4],6))
