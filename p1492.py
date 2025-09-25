341340f4a7d31e1bfb45ebb6d33dc6a999fc02bc
Axiza2k5 <46486645+Axiza2k5@users.noreply.github.com>
2025-09-25 23:20:44 +0700

    1492: AC 100% 68%

---
+++ p1492.py
@@ -0,0 +1,11 @@
+import math
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        count = 0
+        for i in range(1, n):
+            if n%i == 0:
+                count += 1
+                if count == k:
+                    return i
+
+        return -1
