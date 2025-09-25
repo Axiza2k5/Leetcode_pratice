cc058f9d5c73e661d31b5dcfb58a08e1e4557d33
Axiza2k5 <46486645+Axiza2k5@users.noreply.github.com>
2025-09-25 19:54:18 +0700

    605: AC 95% 54%

---
+++ p605.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed, n: int) -> bool:
+        i = 0
+        count = 0
+        ln = len(flowerbed)
+        while i < ln:
+            if flowerbed[i]:
+                i += 2
+                continue
+
+            if i == ln-1 and flowerbed[i]:
+                count += 1 
+                break
+
+            if flowerbed[i] == 0 and flowerbed[i+1]:
+                i += 3
+                continue
+
+            flowerbed[i] = 1
+            count += 1
+            i += 2
+
+        return n <= count
+
