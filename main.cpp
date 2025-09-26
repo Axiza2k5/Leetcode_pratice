29b441f432d052f141c0d0da56e9d7a2d278f211
Axiza2k5 <46486645+Axiza2k5@users.noreply.github.com>
2025-09-26 21:55:39 +0700

    Create: main file for c++

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "p11.cpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    Solution s;
+    vector<int> v = {1,8,6,2,5,4,8,3,7};
+    cout<<(s.maxArea(v));
+
+}
